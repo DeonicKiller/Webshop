d9054f7514c67d9fec8751ded054e9352cdee1aa
Marco van Setten <s1114352@student.windesheim.nl>
2018-11-15 15:22:16 +0100

    user story 2 goeie

---
+++ public/js/webshop.js
@@ -0,0 +1,29 @@
+function getAllProducts() {
+    var xHttp = new XMLHttpRequest();
+    xHttp.onreadystatechange = function () {
+        if (xHttp.readyState == XMLHttpRequest.DONE) {
+            if (xHttp.status == 200 || xHttp.status == 201) {
+                var response = JSON.parse(xHttp.response);
+                // succes
+            } else {
+                // failed
+            }
+        }
+    };
+    xHttp.onerror = function () {
+        // failed
+    };
+    xHttp.open("GET", "api/products", true);
+    xHttp.setRequestHeader('Content-Type', 'application/json');
+    xHttp.send();
+}
+       
+var products = [ 
+    { id: 1, name: "product 1"},
+    { id: 2, name: "product 2"},
+    { id: 3, name: "product 3"}
+]; 
+
+products.forEach(function(value) {
+    console.log(value.name);
+});

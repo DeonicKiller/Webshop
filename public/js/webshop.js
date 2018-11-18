7292294488c3afe7fc715d61574de4a7f70f664b
oussamaidbid <s1133773@student.windesheim.nl>
2018-11-19 00:40:27 +0100

    added seperate webshop and about us page. added animations to icons

--- public/js/webshop.js
+++ public/js/webshop.js
@@ -19,35 +19,55 @@ function getAllProducts() {
     xHttp.setRequestHeader('Content-Type', 'application/json');
     xHttp.send();
 }
-       
-var Products = [ 
-    { id: 1, name: "Grand theft auto V"},
-    { id: 2, name: "Call of Duty IIII"},
-    { id: 3, name: "Rust"},
-    { id: 4, name: "Subnautica"},
-    { id: 5, name: "Cities Skylines"},
-]; 
+
+var Products = [{
+        id: 1,
+        name: "Grand theft auto V"
+    },
+    {
+        id: 2,
+        name: "Call of Duty IIII"
+    },
+    {
+        id: 3,
+        name: "Rust"
+    },
+    {
+        id: 4,
+        name: "Subnautica"
+    },
+    {
+        id: 5,
+        name: "Cities Skylines"
+    },
+];
 
 var p1 = document.getElementById("Product1");
 var p2 = document.getElementById("Product2");
 var p3 = document.getElementById("Product3");
 var p4 = document.getElementById("Product4");
 var p5 = document.getElementById("Product5");
-Products.forEach(function(value) {
+Products.forEach(function (value) {
     console.log(value.name);
 
-    switch(value.id){
-        case 1: p1.innerHTML = value.name;
-        break;
-        case 2: p2.innerHTML = value.name;
-        break;
-        case 3: p3.innerHTML = value.name;
-        break;
-        case 4: p4.innerHTML = value.name;
-        break;
-        case 5: p5.innerHTML = value.name;
-        break;
-        default: error;
+    switch (value.id) {
+        case 1:
+            p1.innerHTML = value.name;
+            break;
+        case 2:
+            p2.innerHTML = value.name;
+            break;
+        case 3:
+            p3.innerHTML = value.name;
+            break;
+        case 4:
+            p4.innerHTML = value.name;
+            break;
+        case 5:
+            p5.innerHTML = value.name;
+            break;
+        default:
+            error;
 
 
 
@@ -56,43 +76,102 @@ Products.forEach(function(value) {
 
 
 // Voor de Layout
-
+var homeLogo = document.getElementById("home-logo");
 var selectionImg1 = document.getElementById("selection-1");
 var selectionImg2 = document.getElementById("selection-2");
+var selectionImg3 = document.getElementById("selection-3");
 var overlayText1 = document.getElementById("selection-1-text");
 var overlayText2 = document.getElementById("selection-2-text");
+var overlayText3 = document.getElementById("selection-3-text");
 
 
-function showOverlay1(){
+var homePage = document.getElementById("home-page");
+var webshopPage = document.getElementById("webshop-page");
+var aboutusPage = document.getElementById("aboutus-page");
+
+
+
+
+function showOverlay1() {
 
     selectionImg1.style.opacity = 0.3;
-    overlayText1.style.opacity = 1;   
+    overlayText1.style.opacity = 1;
 }
 
-function showOverlay2(){
+function showOverlay2() {
 
     selectionImg2.style.opacity = 0.3;
     overlayText2.style.opacity = 1;
 }
-function hideOverlay1(){
+
+function showOverlay3() {
+
+    selectionImg3.style.opacity = 0.3;
+    overlayText3.style.opacity = 1;
+}
+
+function hideOverlay1() {
 
     selectionImg1.style.opacity = 1;
     overlayText1.style.opacity = 0;
 }
-function hideOverlay2(){
+
+function hideOverlay2() {
 
     selectionImg2.style.opacity = 1;
     overlayText2.style.opacity = 0;
 }
 
-function addHomePageActions(){
+function hideOverlay3() {
+
+    selectionImg3.style.opacity = 1;
+    overlayText3.style.opacity = 0;
+}
+
+function switchPage(hidePage, showPage) {
+
+    hidePage.style.display = "none";
+    showPage.style.display = "block";
+
+}
+
+function hidePages() {
+    webshopPage.style.display = "none";
+    aboutusPage.style.display = "none";
+}
+
+function addHomePageActions() {
 
     selectionImg1.addEventListener("mouseover", showOverlay1);
     selectionImg2.addEventListener("mouseover", showOverlay2);
+    selectionImg3.addEventListener("mouseover", showOverlay3);
 
     selectionImg1.addEventListener("mouseout", hideOverlay1);
     selectionImg2.addEventListener("mouseout", hideOverlay2);
+    selectionImg3.addEventListener("mouseout", hideOverlay3);
+
+    selectionImg1.addEventListener("click", function () {
+        switchPage(homePage, webshopPage);
+
+    });
+    selectionImg2.addEventListener("click", function () {
+
+        switchPage(homePage, aboutusPage);
+
+    });
+
+    homeLogo.addEventListener("click", function(){
+
+
+        if(webshopPage.style.display == "block"){
+            switchPage(webshopPage, homePage);
+        }
+        else if(aboutusPage.style.display == "block"){
+            switchPage(aboutusPage, homePage);
+        }
+    });
 
 }
 
 addHomePageActions();
+hidePages();

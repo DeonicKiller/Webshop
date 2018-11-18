bc838a7f2250b6e2e66b92a9750898fa3e5230ff
Marco van Setten <s1114352@student.windesheim.nl>
2018-11-18 14:26:42 +0100

    changes

--- public/js/webshop.js
+++ public/js/webshop.js
@@ -40,11 +40,11 @@ var p3 = document.getElementById("Product3");
 var p4 = document.getElementById("Product4");
 var p5 = document.getElementById("Product5");
 
-p1.innerHTML = ()
-p2.innerHTML = ()
-p3.innerHTML = ()
-p4.innerHTML = ()
-p5.innerHTML = ()
+p1.innerHTML = ();
+p2.innerHTML = ();
+p3.innerHTML = ();
+p4.innerHTML = ();
+p5.innerHTML = ();
 // Voor de Layout
 
 var selectionImg1 = document.getElementById("selection-1");

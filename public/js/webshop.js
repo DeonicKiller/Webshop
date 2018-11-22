d122559c96c545a3dd8310d38bfea99508d7be7a
Marco van Setten <s1114352@student.windesheim.nl>
2018-11-22 11:56:00 +0100

    aangepast

--- public/js/webshop.js
+++ public/js/webshop.js
@@ -75,7 +75,228 @@ var productPrijs1 = document.getElementById("")
 Products.forEach(function (value) {
     console.log(value.name);
 
-    switch (value.id) {
+    switch (value.id) {//API voor de producten
+
+        function getAllProducts() {
+            var xHttp = new XMLHttpRequest();
+            xHttp.onreadystatechange = function () {
+                if (xHttp.readyState == XMLHttpRequest.DONE) {
+                    if (xHttp.status == 200 || xHttp.status == 201) {
+                        var response = JSON.parse(xHttp.response);
+                        // succes
+                    } else {
+                        // failed
+                    }
+                }
+            };
+            xHttp.onerror = function () {
+                // failed
+            };
+            xHttp.open("GET", "api/products", true);
+            xHttp.setRequestHeader('Content-Type', 'application/json');
+            xHttp.send();
+        }
+        
+        var euroTeken = "<p>&euro;</p>";
+        
+        var Products = [{
+                id: 1,
+                name: "Grand theft auto V",
+                prijs: euroTeken + 59.98,
+                beschrijving: "PS4",
+                image: '<img  class="product-image" src="img/GTAV_PS4.jpg" alt="webshop">'
+                
+            },
+            {
+                id: 2,
+                name: "Call of Duty IIII",
+                prijs: euroTeken + 59.98,
+                beschrijving: "PS4",
+                image: '<img  class="product-image" src="img/COD4_PS4.jpg" alt="webshop">'
+            },
+            {
+                id: 3,
+                name: "Assassin's Creed: Odyssey",
+                prijs: euroTeken + 59.98,
+                beschrijving: "PS4",
+                image: '<img  class="product-image" src="img/ACODYSSEY_PS4.jpg" alt="webshop">'
+            },
+            {
+                id: 4,
+                name: "Red Dead Redemption II",
+                prijs: euroTeken + 59.98,
+                beschrijving: "PS4",
+                image: '<img  class="product-image" src="img/RDR2_PS4.jpg" alt="webshop">'
+            },
+            {
+                id: 5,
+                name: "Spider-Man",
+                prijs: euroTeken + 59.98,
+                beschrijving: "PS4",
+                image: '<img  class="product-image" src="img/Spiderman_PS4.jpg" alt="webshop">'
+            },
+        ];
+        
+        var p1 = document.getElementById("Product1");
+        var p2 = document.getElementById("Product2");
+        var p3 = document.getElementById("Product3");
+        var p4 = document.getElementById("Product4");
+        var p5 = document.getElementById("Product5");
+        
+        var productNaam1 = document.getElementById("name-1");
+        var productNaam2 = document.getElementById("name-2");
+        var productNaam3 = document.getElementById("name-3");
+        var productNaam4 = document.getElementById("name-4");
+        var productNaam5 = document.getElementById("name-5");
+        var productPrijs1 = document.getElementById("")
+        Products.forEach(function (value) {
+            console.log(value.name);
+        
+            switch (value.id) {
+                case 1:
+                    //p1.innerHTML = value.name + " " + value.prijs + " " + value.beschrijving + value.image;
+        
+                    productNaam1.innerHTML = value.name;
+                    productPrijs.inner = value.prijs
+                    break;
+                case 2:
+                    //p2.innerHTML = value.name + " " + value.prijs + " " + value.beschrijving + value.image;
+        
+                    productNaam2.innerHTML = value.name;
+                    break;
+                case 3:
+                    //p3.innerHTML = value.name + " " + value.prijs + " " + value.beschrijving + value.image;
+        
+                    productNaam3.innerHTML = value.name;
+                    break;
+                case 4:
+                    //p4.innerHTML = value.name + " " + value.prijs + " " + value.beschrijving + value.image;
+        
+                    productNaam4.innerHTML = value.name;
+                    break;
+                case 5:
+                    //p5.innerHTML = value.name + " " + value.prijs + " " + value.beschrijving + value.image;
+        
+                    productNaam5.innerHTML = value.name;
+                    break;
+                default:
+                    error;
+        
+        
+        
+            }
+        });
+        
+        
+        // Voor de Layout
+        var homeLogo = document.getElementById("home-logo");
+        var selectionImg1 = document.getElementById("selection-1");
+        var selectionImg2 = document.getElementById("selection-2");
+        var selectionImg3 = document.getElementById("selection-3");
+        var overlayText1 = document.getElementById("selection-1-text");
+        var overlayText2 = document.getElementById("selection-2-text");
+        var overlayText3 = document.getElementById("selection-3-text");
+        
+        
+        var homePage = document.getElementById("home-page");
+        var webshopPage = document.getElementById("webshop-page");
+        var aboutusPage = document.getElementById("aboutus-page");
+        
+        
+        
+        
+        function showOverlay1() {
+        
+            selectionImg1.style.opacity = 0.3;
+            overlayText1.style.opacity = 1;
+        }
+        
+        function showOverlay2() {
+        
+            selectionImg2.style.opacity = 0.3;
+            overlayText2.style.opacity = 1;
+        }
+        
+        function showOverlay3() {
+        
+            selectionImg3.style.opacity = 0.3;
+            overlayText3.style.opacity = 1;
+        }
+        
+        function hideOverlay1() {
+        
+            selectionImg1.style.opacity = 1;
+            overlayText1.style.opacity = 0;
+        }
+        
+        function hideOverlay2() {
+        
+            selectionImg2.style.opacity = 1;
+            overlayText2.style.opacity = 0;
+        }
+        
+        function hideOverlay3() {
+        
+            selectionImg3.style.opacity = 1;
+            overlayText3.style.opacity = 0;
+        }
+        
+        function switchPage(hidePage, showPage) {
+        
+            hidePage.style.display = "none";
+            showPage.style.display = "block";
+        
+        }
+        
+        function hidePages() {
+            webshopPage.style.display = "none";
+            aboutusPage.style.display = "none";
+        }
+        
+        function addHomePageActions() {
+        
+            selectionImg1.addEventListener("mouseover", showOverlay1);
+            selectionImg2.addEventListener("mouseover", showOverlay2);
+            selectionImg3.addEventListener("mouseover", showOverlay3);
+        
+            selectionImg1.addEventListener("mouseout", hideOverlay1);
+            selectionImg2.addEventListener("mouseout", hideOverlay2);
+            selectionImg3.addEventListener("mouseout", hideOverlay3);
+        
+            selectionImg1.addEventListener("click", function () {
+                switchPage(homePage, webshopPage);
+        
+            });
+            selectionImg2.addEventListener("click", function () {
+        
+                switchPage(homePage, aboutusPage);
+        
+            });
+        
+            homeLogo.addEventListener("click", function(){
+        
+        
+                if(webshopPage.style.display == "block"){
+                    switchPage(webshopPage, homePage);
+                }
+                else if(aboutusPage.style.display == "block"){
+                    switchPage(aboutusPage, homePage);
+                }
+            });
+        
+            homeLogo.addEventListener("mouseover", function(){
+        
+                homeLogo.style.opacity = 0.3;
+        
+            });
+            homeLogo.addEventListener("mouseout", function(){
+                homeLogo.style.opacity = 1;
+            });
+        
+        }
+        
+        addHomePageActions();
+        hidePages();
         case 1:
             //p1.innerHTML = value.name + " " + value.prijs + " " + value.beschrijving + value.image;
 

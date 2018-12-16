15af36817fc6e46311a61fda9f8d96ef1aea0c25
oussamaidbid <s1133773@student.windesheim.nl>
2018-12-16 18:07:30 +0100

    added product class

---
+++ public/js/Product.js
@@ -0,0 +1,31 @@
+class Product{
+
+    setName(name){
+        this.name = name;
+    }
+    setPrice(price){
+        this.price = price;
+    }
+    setPlatform(platform){
+        this.platform = platform;
+    }
+    setImage(image){
+        this.image = image;
+    }
+
+    getName(){
+        return this.name;
+    }
+    getPrice(){
+        return this.price;
+    }
+    getPlatform(){
+        return this.platform;
+    }
+    getImage(){
+        return this.image;
+    }
+
+    
+
+}
